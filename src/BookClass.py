49c6a10337b500ff84e54554920f02db751e7b58
Antariksh Bothale <antariksh.bothale@gmail.com>
2014-05-09 17:09:59 -0700

    Just testing. Will write code next.

---
+++ src/BookClass.py
@@ -0,0 +1,6 @@
+# LING 575 Sentiment Project
+# BookReviewClass.py Defines a class that represents a single BookReview
+# Also provides functions for reading reviews from the file and pickling them
+
+__author__ = 'Antariksh Bothale'
+
