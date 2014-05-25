db557bc4effa5669c18cd6cdd7d4c0b2fb9ee525
maria-antoniak <riamarie@uw.edu>
2014-05-25 10:07:49 -0700

    Improved MALLET LDA scripts

--- src/make_vectors/run_lda.sh
+++ src/make_vectors/run_lda.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mallet train-topics --input train.vectors.mallet --num-topics 20
+mallet train-topics --input train.vectors.mallet --num-topics 10 --optimize-interval 10 --output-topic-keys topics.10.txt --output-doc-topics topics.10.composition.txt
