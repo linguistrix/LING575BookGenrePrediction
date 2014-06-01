d3ee8f87b4b184f182d6afccbdda2c5771d16703
maria-antoniak <riamarie@uw.edu>
2014-05-31 18:58:43 -0700

    Fixed unique ID

--- src/make_vectors/make_sentence_vectors.py
+++ src/make_vectors/make_sentence_vectors.py
@@ -2,14 +2,16 @@ __author__ = 'Maria Antoniak'
 
 import sys, pickle
 import re, nltk
-import string
+
 
 def main():
     input_file = open(sys.argv[1], 'r')
     output_file = open(sys.argv[2], 'w')
     reviews = pickle.load(input_file)
+    j = 0
     for review in reviews:
-        unique_id = review.productID + review.userID
+        unique_id = review.productID + review.userID + j
+        j += 1
         main_genre = review.genre.split()[0].replace(',', '')
         tokens = nltk.word_tokenize(review.text.lower())
         sentence_vectors = []
--- src/make_vectors/make_vectors.py
+++ src/make_vectors/make_vectors.py
@@ -8,8 +8,10 @@ def main():
     input_file = open(sys.argv[1], 'r')
     output_file = open(sys.argv[2], 'w')
     reviews = pickle.load(input_file)
+    j = 0
     for review in reviews:
-        unique_id = review.productID + review.userID
+        unique_id = review.productID + review.userID + j
+        j += 1
         main_genre = review.genre.split()[0].replace(',', '')
         text_without_punct = re.sub(r'[^\w\s_]', '', review.text.lower())
         tokens = nltk.word_tokenize(text_without_punct)
